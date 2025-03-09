e5224a3ac27cb7e5249ca2e7f1afb650c57ee68c
Zhou-Shude <138975672+shude1999@users.noreply.github.com>
2025-03-09 20:55:37 +0800

    Tile Autopositioning script

--- Elderly game/Assets/Script/beat.cs
+++ Elderly game/Assets/Script/beat.cs
@@ -7,9 +7,17 @@ public class beat : MonoBehaviour
     // Start is called before the first frame update
     public float beatTempo;
     public bool hasStart = false;
+    public GameObject tile;
+    public GameObject RedFinal;
+    public GameObject YellowFinal;
+    public GameObject BlueFinal;
+    public GameObject GreenFinal;
+
+
     void Start()
     {
         beatTempo = beatTempo / 60f;
+        PositionIni("grybgrybgryb");
     }
 
     // Update is called once per frame
@@ -25,6 +33,45 @@ public class beat : MonoBehaviour
         else
         {
             transform.position += new Vector3(0f, beatTempo * Time.deltaTime, 0f);
+        }
+    }
+
+    void PositionIni(string a)
+    {
+        string b = a.ToUpper();
+        int alength = a.Length;
+        for (int i = 1; i < alength + 1; i++)
+        {
+            char c = b[i - 1];
+            if (c == 'R')
+            {
+                GameObject buttonz = tile.transform.GetChild(i).gameObject;
+                GameObject actualbutton = buttonz.transform.GetChild(2).gameObject;
+                actualbutton.SetActive(true);
+                actualbutton.transform.position = new Vector3(RedFinal.transform.position.x, -4 - (i-1)*4,-0); 
+            }
+            if (c == 'B')
+            {
+                GameObject buttonz = tile.transform.GetChild(i).gameObject;
+                GameObject actualbutton = buttonz.transform.GetChild(0).gameObject;
+                actualbutton.SetActive(true);
+                actualbutton.transform.position = new Vector3(BlueFinal.transform.position.x, -4 - (i - 1) * 4, -0);
+            }
+
+            if (c == 'Y')
+            {
+                GameObject buttonz = tile.transform.GetChild(i).gameObject;
+                GameObject actualbutton = buttonz.transform.GetChild(3).gameObject;
+                actualbutton.SetActive(true);
+                actualbutton.transform.position = new Vector3(YellowFinal.transform.position.x, -4 - (i - 1) * 4, -0);
+            }
+            if (c == 'G')
+            {
+                GameObject buttonz = tile.transform.GetChild(i).gameObject;
+                GameObject actualbutton = buttonz.transform.GetChild(1).gameObject;
+                actualbutton.SetActive(true);
+                actualbutton.transform.position = new Vector3(GreenFinal.transform.position.x, -4 - (i - 1) * 4, -0);
+            }
         }
     }
 }
