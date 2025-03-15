2815e6f27775f18b82fe044c860edb75a54c72b2
Green-Tea-123 <junboonsoo@gmail.com>
2025-03-16 00:23:35 +0800

    Update bluetooth API file locations

--- Elderly game/Assets/Script/Bluetooth Scripts/BluetoothManager.cs
+++ Elderly game/Assets/Script/Bluetooth Scripts/BluetoothManager.cs
@@ -1,8 +1,44 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using System.Threading.Tasks;
+using ArduinoBluetoothAPI;
 
 public class BluetoothManager : MonoBehaviour {
+
+    private BluetoothHelper bluetoothHelper;
+
+    private string deviceName;
     void Start() {
+        this.deviceName = "HC-06";
+        try {
+            bluetoothHelper = BluetoothHelper.GetInstance(deviceName);
+            bluetoothHelper.OnConnected += OnConnected;
+            bluetoothHelper.OnConnectionFailed += OnConnectionFailed;
+            bluetoothHelper.setTerminatorBasedStream("\n");
+            if (bluetoothHelper.isDevicePaired()) {
+                bluetoothHelper.Connect();
+            }
+            
+        } catch (System.Exception e) {
+            Debug.Log("Error: " + e.Message);
+        }
+    }
+
+    private void OnConnected(BluetoothHelper bluetoothHelper) {
+        bluetoothHelper.StartListening();
+    }
+
+    private void OnConnectionFailed(BluetoothHelper bluetoothHelper) {
+
+    }
+
+    void Update() {
+        if (bluetoothHelper.Available) {
+            string msg = bluetoothHelper.Read();
+        }
+    }
+
+    void CloseConnection() {
+        bluetoothHelper.Disconnect();
     }
 }
